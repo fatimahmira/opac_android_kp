49b72193fb6015286df45d777b2eedee9c992a04
fatimahmira <49262430+fatimahmira@users.noreply.github.com>
2020-03-03 11:41:24 +0700

    commit pertama

---
+++ android/app/src/main/kotlin/com/example/opac_android_kp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.opac_android_kp
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
